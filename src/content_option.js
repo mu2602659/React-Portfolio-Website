efca66b52465867962409d1ccbc42e35094ece72
Muhammad Uzair <mu2602659@gmail.com>
2024-01-12 18:34:30 +0500

    changed

---
+++ src/content_option.js
@@ -0,0 +1,165 @@
+const logotext = "UZAIR";
+const meta = {
+    title: "Uzair's Portfolio",
+    description: "I’m Uzair _ A Full Stack developer,",
+};
+
+const introdata = {
+    title: "I’m Muhammad Uzair",
+    animated: {
+        first: "I love coding",
+        second: "I develop cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "A tech enthusiast shaping the digital landscape with expertise in full stack, fueled by a passion for web development and a proven track record from internships to freelance projects.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Muhammad Uzair",
+    aboutme: "Hello, I'm Muhammad Uzair – a passionate and dedicated Information Engineering Technology student at Superior University. With a solid foundation in computer science since my matriculation at The Spirit School, I've honed my skills in HTML, CSS, Bootstrap, JavaScript, TypeScript, React.js, C++, and Oracle. My heart lies in web development, and the MERN stack is where I thrive. During my educational journey, I've complemented my theoretical knowledge with real-world experience. I've immersed myself in the industry through internships, spending 6 months as a data engineer at AI Studio and gaining valuable insights as a frontend developer and React.js developer during shorter stints. Freelancing has been my playground, where I've translated my skills into practical solutions. From crafting engaging landing pages to dynamic web applications using React.js, I've explored the vast landscape of web development. Currently delving into the realms of Node.js, Express.js, and MongoDB, I'm focused on expanding my expertise. My sights are set on my final year project – an innovative mobile app for service providers developed on React Native. Join me on my journey as I combine passion with proficiency, turning ideas into immersive digital experiences. Let's create something extraordinary together.",};
+
+export    const interest = [
+    {
+        language: "MERN Stack",
+        
+    },
+    {
+        language: "MEAN Stack",
+        
+    },
+    {
+        language: "MEVN Stack",
+        
+    },
+];
+
+
+    const worktimeline = [
+    {
+        jobtitle: "React JS Intern",
+        where: "Pursue Today",
+        date: "2023",
+    },
+    {
+        jobtitle: "HTML, CSS, Bootstrap Intern",
+        where: "Aresync",
+        date: "2023",
+    },
+    {
+        jobtitle: "Data Engineer",
+        where: "AI Studio",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "Bootstrap",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 50,
+    },
+{
+        name: "Typescript",
+        value: 50,
+    },
+    {
+        name: "Oracle",
+        value: 80,
+    },
+    {
+        name: "Python",
+        value: 60,
+    },
+{
+        name: "C++",
+        value: 60,
+    },
+    {
+        name: "React Native",
+        value: 60,
+    },
+    {
+        name: "Git",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "Landing Pages",
+        description: "Elevate your brand with captivating landing pages crafted by Muhammad Uzair, merging design finesse with cutting-edge web development expertise.",
+    },
+    {
+        title: "Websites",
+        description: "Elevating businesses through expertly crafted websites, I specialize in delivering seamless user experiences and visually stunning designs",
+    },
+    {
+        title: "Full Stack Web Applications",
+        description: "Crafting seamless and scalable full-stack web applications to bring your digital ideas to life.",
+    },
+    {
+        title: "Mobile App",
+        description: "Transforming ideas into seamless mobile experiences – crafting innovative and user-friendly apps tailored to your needs",
+    },
+];
+
+const dataportfolio = [{
+    "img": "/images/Uzdoc.png",
+    "description": "A hospital management system for efficient patient care.",
+    "link": "https://mu2602659.github.io/Uzdoc-Hospital-Management-System/"
+},
+
+    {
+   "img": "/images/portfolio.png",
+        description: "A portfolio web design with innovative solutions.",
+        link: "https://mu2602659.github.io/UZAIRTECH.com/"
+    },
+    ];
+
+const contactConfig = {
+    YOUR_EMAIL: "mu2602659@gmail.com",
+    YOUR_FONE: "+923354677774",
+    description: "Let's connect! Reach out for collaborations, inquiries, or just to say hello. I'm eager to discuss how we can turn your ideas into digital realities. ",
+    // create an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/mu2602659",
+    facebook: "https://www.facebook.com/MalixxUzair?mibextid=kFxxJD",
+    linkedin: "https://www.linkedin.com/in/muhammad-uzair-858753259",
+    instagram: "https://www.instagram.com/_uzair.x_36?igsh=MWxzdHBmYnA0b251aw==",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
